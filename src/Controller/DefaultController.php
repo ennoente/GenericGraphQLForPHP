36f36c6e27fedb0cc6a1ea64e815c66cea22c56c
Enno Thoma <enno.thoma@uni-oldenburg.de>
2020-01-06 23:46:48 +0100

    Add rudimentary many-to-many support. Still a lot to do.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use GraphQL\Language\Parser;
+use GraphQL\Language\Source;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,14 @@ class DefaultController extends AbstractController
     private $selectedObjects = array();
 
     private $result = [];
+
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = mysqli_connect("127.0.0.1", "", "", "brabo_two", "3306");
+        $this->connection->set_charset("utf8");
+    }
 
     /**
      * @Route(path="/graphql", methods={"POST"})
@@ -35,8 +44,27 @@ class DefaultController extends AbstractController
         /** Return the DocumentNode calculated from the request query */
         //return new JsonResponse($docNodeAsResource);
 
+        //return new JsonResponse(self::columnExistsInTable("sys_hersteller", "sys_maschine"));
+        //return new JsonResponse(self::getCombinedTableName("sys_maschine", "document"));
+
         /** Return the first definition */
         //return new JsonResponse($definitions[0]);
+
+        //$this->columnExistsInTable("name", "sys_maschine");
+        //$this->columnExistsInTable("id", "sys_maschine");
+        //return new JsonResponse(["queriedColumnsInTable" => $this->queriedColumnsInTable,
+        //    "bla" => array_key_exists("sys_maschine", $this->queriedColumnsInTable),
+        //    "bla2" => in_array("name", $this->queriedColumnsInTable["sys_maschine"])]);
+        //return new JsonResponse($this->columnExistsInTable("name", "sys_maschine"));
+
+        /*
+        $arr = [];
+        $arr["obj"] = [];
+        array_push($arr["obj"], "Hallo");
+        array_push($arr["obj"], "Hallo2");
+        return new JsonResponse($arr);
+        */
+
 
         /** Return the calculated JSON response structure */
         //return new JsonResponse($this->returnJson($definitions[0]));
@@ -47,31 +75,39 @@ class DefaultController extends AbstractController
         //$query = $this->returnQuerySQL($definitions[0])[0];
         //return new JsonResponse(["query" => $query]);
 
-        $connection = mysqli_connect("127.0.0.1", "", "", "brabo_two", "3306");
-        //$connection = mysqli_connect("127.0.0.1", "", "", "graphqldb", "3306");
-        $connection->set_charset("utf8");
+        //$this->connection = mysqli_connect("127.0.0.1", "", "", "graphqldb", "3306");
 
         $response = [];
-        $queries = $this->returnQuerySQL($toplevelObject);
 
+        //$queries = $this->returnQuerySQL($toplevelObject);
+
+
+        //for ($i = 0; $i < count($toplevelSelections); $i++) {
+        //for ($j = 0; $j < count($queries); $j++) {
         for ($j = 0; $j < count($toplevelSelections); $j++) {
             $current = [];
+            $this->selectedObjects = [];
 
-            $query = $queries[$j];
-            $result = $connection->query($query);
+            //$query = $queries[$j];
+            $query = $this->returnSQL($toplevelSelections[$j]);
 
-            if ($error = $connection->error)
+            //$result = $this->connection->query($query);
+            $result = $this->queryDb($query);
+
+            if ($error = $this->connection->error)
                 return new JsonResponse(["error" => $error], Response::HTTP_UNPROCESSABLE_ENTITY);
 
             $this->result = $result->fetch_assoc();
 
             for ($i = 0; $i < $result->num_rows; $i++) {
-                array_push($current, $this->returnJson($toplevelSelections[$j], 0, $toplevelSelections[$j]["name"]["value"]));
+                array_push($current, $this->returnJson($toplevelSelections[$j], 0, $toplevelSelections[$j]["name"]["value"], $this->result));
                 $this->result = $result->fetch_assoc();
             }
             $response[$toplevelSelections[$j]["name"]["value"]] = $current;
         }
+        //}
 
+        //return new JsonResponse(["connectionStats" => $this->connection->get_connection_stats(), "count" => $this->dbCount, "data" => $response]);
         return new JsonResponse(["data" => $response]);
         //return new JsonResponse($this->returnJson($definitions[0]));
 
@@ -80,7 +116,7 @@ class DefaultController extends AbstractController
         //return new JsonResponse(["data" => $this->returnJson($definitions[0])]);
         //return new JsonResponse([$assoc, array_keys($assoc), $values, "errorKey" => $errorKey]);
 
-        //return new JsonResponse($connection->query($query)->num_rows);
+        //return new JsonResponse($this->connection->query($query)->num_rows);
 
     }
 
@@ -91,9 +127,98 @@ class DefaultController extends AbstractController
         $selections = $queryDefinitions["selectionSet"]["selections"];
         for ($i = 0; $i < count($selections); $i++) {
             array_push($sqlQueries, $this->returnSQL($selections[$i]));
-            $this->selectedObjects = [];
+            //$this->selectedObjects = [];
         }
         return $sqlQueries;
+    }
+
+    private $queriedColumnsInTable = [];
+    private $columnNotInTable = [];
+
+    function columnExistsInTable($columnName, $tableName): bool
+    {
+        if (array_key_exists($tableName, $this->queriedColumnsInTable))
+            if (in_array($columnName, $this->queriedColumnsInTable[$tableName])) return true;
+
+        //var_dump("_______");
+        //var_dump(array_key_exists($tableName, $this->columnNotInTable));
+
+        if (array_key_exists($tableName, $this->columnNotInTable)) {
+            //var_dump("$columnName exists in columnNotInTable: " . in_array($columnName, $this->columnNotInTable));
+            if (in_array($columnName, $this->columnNotInTable[$tableName])) {
+                //var_dump("SQL");
+                return false;
+            }
+        }
+
+        $sql = "SHOW COLUMNS FROM " . $tableName . " LIKE " . "'" . $columnName . "'";
+
+        //$results = $this->connection->query($sql);
+        $results = $this->queryDb($sql);
+
+        //var_dump($this->columnNotInTable);
+        //var_dump(array_key_exists($columnName, $this->columnNotInTable));
+
+        if ($results->num_rows > 0) {
+            if (!array_key_exists($tableName, $this->queriedColumnsInTable)) {
+                //array_push($this->queriedColumnsInTable, $tableName => array());
+                $this->queriedColumnsInTable[$tableName] = [];
+                //var_dump($this->queriedColumnsInTable);
+            } else
+                //var_dump("In queried columns!");
+
+                //$this->queriedColumnsInTable[$tableName] = $columnName;
+                array_push($this->queriedColumnsInTable[$tableName], $columnName);
+
+            return true;
+        } else {
+            if (!array_key_exists($tableName, $this->columnNotInTable)) {
+                //var_dump("Adding $tableName");
+                $this->columnNotInTable[$tableName] = [];
+            }
+            //var_dump("Adding columnName $columnName to table $tableName");
+            array_push($this->columnNotInTable[$tableName], $columnName);
+            //var_dump($this->columnNotInTable);
+        }
+
+        return false;
+        //return $results->num_rows > 0;
+    }
+
+    private $tableExistsInDatabaseCount = 0;
+
+    private $tablesInDb = [];
+    private $tablesNotInDb = [];
+
+    function tableExistsInDatabase($tableName): bool
+    {
+        if (in_array($tableName, $this->tablesInDb)) return true;
+        if (in_array($tableName, $this->tablesNotInDb)) return false;
+
+
+        $this->tableExistsInDatabaseCount++;
+        $sql = "SHOW TABLES LIKE '$tableName'";
+
+        //$result = $this->connection->query($sql);
+        $result = $this->queryDb($sql);
+        //return !is_null($result);
+
+        // Tables seems to exist
+        if ($result->num_rows > 0) {
+            array_push($this->tablesInDb, $tableName);
+            return true;
+        } else {
+            array_push($this->tablesNotInDb, $tableName);
+            return false;
+        }
+    }
+
+    private $dbCount = 0;
+
+    function queryDb($sql)
+    {
+        $this->dbCount++;
+        return $this->connection->query($sql);
     }
 
 
@@ -102,6 +227,9 @@ class DefaultController extends AbstractController
         $sql = $this->addSqlFields($queryNode);
         $sql .= " FROM " . $queryNode["name"]["value"];
         $sql .= $this->addJoins();
+
+        if (self::hasArgument($queryNode, "id"))
+            $sql .= $this->addIdClause($queryNode["name"]["value"], self::getArgumentValue($queryNode, "id"));
 
         if (self::hasArgument($queryNode, "first"))
             $sql .= $this->addLimit(self::getArgumentValue($queryNode, "first"));
@@ -124,7 +252,8 @@ class DefaultController extends AbstractController
         return -1;
     }
 
-    function hasArgument($node, $argumentKey) {
+    function hasArgument($node, $argumentKey)
+    {
         $arguments = $node["arguments"];
 
         foreach ($arguments as $argument) {
@@ -145,14 +274,46 @@ class DefaultController extends AbstractController
         return " OFFSET " . $value;
     }
 
+    function getCombinedTableName($ass1, $ass2)
+    {
+        $combination1 = $ass1 . "_" . $ass2;
+        $combination2 = $ass2 . "_" . $ass1;
+
+        if (self::tableExistsInDatabase($combination1))
+            return $combination1;
+        else if (self::tableExistsInDatabase($combination2))
+            return $combination2;
+
+        return "ERROR";
+    }
+
 
     function addJoins()
     {
         $sql = "";
         foreach ($this->selectedObjects as $obj) {
-            $sql .= " LEFT JOIN " . $obj["to"] . " ON " . $obj["to"] . ".id = " . $obj["from"] . "." . $obj["to"] . "_id";
+            switch ($obj["type"]) {
+                case "one-to-one":
+                    $sql .= " LEFT JOIN " . $obj["to"] . " ON " . $obj["to"] . ".id = " . $obj["from"] . "." . $obj["to"] . "_id";
+                    break;
+
+                /*
+            case "many-to-many":
+                $from = $obj["from"];
+                $to = $obj["to"];
+                $combinedTableName = self::getCombinedTableName($from, $to);
+                $sql .= " LEFT JOIN $combinedTableName ON $from" . ".id = $combinedTableName" . "." . $from . "_id";
+                $sql .= " LEFT JOIN $to ON $combinedTableName" . "." . $to . "_id = " . $to . ".id";
+                */
+            }
         }
         return $sql;
+    }
+
+
+    function addIdClause($nodeName, $id)
+    {
+        return " WHERE " . $nodeName . ".id" . " = " . $id;
     }
 
 
@@ -163,14 +324,33 @@ class DefaultController extends AbstractController
         //$sql .= "SELECT ";
         //return $selections;
         // Add fields to SQL query
+
+        $sql .= $isFirstField ?
+            $rootFieldName . "." . "id" :
+            ", " . $rootFieldName . "." . "id";
+
+        $sql .= " AS " . "'" . $rootFieldName . "." . "id" . "'";
+        $isFirstField = false;
+
         for ($i = 0; $i < count($selections); $i++) {
             $field = $selections[$i];
             $fieldName = $field["name"]["value"];
+
+            if ($fieldName == "id") {
+                continue;
+            }
+
             if (self::isSpread($field)) {
-                //array_push($selectedObjects, $field);
-                //array_push($this->selectedObjects, [$rootFieldName => $fieldName]);
-                array_push($this->selectedObjects, ["from" => $rootFieldName, "to" => $fieldName]);
-                $sql .= $this->addSqlFields($field, $branchDepth + 1, "", $isFirstField);
+                // Requested fieldname exists as column in table
+                if (self::columnExistsInTable($fieldName . "_id", $rootFieldName)) {
+                    array_push($this->selectedObjects, ["type" => "one-to-one", "from" => $rootFieldName, "to" => $fieldName]);
+
+                    $sql .= $this->addSqlFields($field, $branchDepth + 1, "", $isFirstField);
+                } else {
+                    // Many to many (for now) TODO
+
+                    array_push($this->selectedObjects, ["type" => "many-to-many", "from" => $rootFieldName, "to" => $fieldName]);
+                }
             } else {
                 // Current node is not object but scalar type field
                 $sql .= $isFirstField ?
@@ -182,29 +362,10 @@ class DefaultController extends AbstractController
             }
         }
         return $sql;
-        /*
-        for ($i = 0; $i < count($selections); $i++) {
-            $field = $selections[$i];
-            $fieldName = $field["name"]["value"];
-            // LEFT JOIN
-            if (self::isSpread($field)) {
-                //$sql .= " LEFT JOIN " . $fieldName . " ON " . $fieldName . "_id" . " = " . $rootFieldName . ".id" . " ";
-                $sql .= " LEFT JOIN " . $fieldName . " ON " . $fieldName . ".id" . " = " . $rootFieldName . "." . $fieldName . "_id";
-            } else {
-                // Current node is not object but scalar type field
-                $sql .= $isFirstField ?
-                    $rootFieldName . "." . $fieldName :
-                    ", " . $rootFieldName . "." . $fieldName;
-                $isFirstField = false;
-            }
-        }
-        $sql .= " FROM " . $rootFieldName;
-        return $sql;
-        */
     }
 
 
-    function returnJson($currentNode, $branchDepth = 0, $parentNodeName = "")
+    function returnJson($currentNode, $branchDepth = 0, $parentNodeName = "", $resultSet = [])
     {
         $selections = $currentNode["selectionSet"]["selections"];
         $newObj = [];
@@ -212,18 +373,80 @@ class DefaultController extends AbstractController
             $field = $selections[$i];
             $fieldName = $field["name"]["value"];
             $arguments = $field["arguments"];
+
             if (self::isSpread($field)) {
-                $newObj[$fieldName] = $this->returnJson($field, $branchDepth + 1, $fieldName);
+                $key = $parentNodeName . "." . $fieldName;
+                //if (array_key_exists($key, $resultSet))
+                if (self::columnExistsInTable($fieldName . "_id", $parentNodeName))
+                    $newObj[$fieldName] = $this->returnJson($field, $branchDepth + 1, $fieldName, $resultSet);
+                else
+                    //$newObj[$fieldName] = $this->addSqlFields($currentNode);
+                    //$newObj[$fieldName] = "My id is '" . $resultSet[$parentNodeName . ".id"] . "', parentNodeName=" . $parentNodeName;
+
+                    //$newObj[$fieldName] = array();
+
+                    $newObj[$fieldName] = self::getManyToManyAsArray($field, $fieldName, $parentNodeName, $resultSet[$parentNodeName . ".id"]);
             } else {
                 //$newObj[$fieldName] = null;
                 //$newObj[$fieldName] = $parentNodeName;
 
-                $newObj[$fieldName] = $this->result[$parentNodeName . "." . $fieldName];
+                //if (array_key_exists($key, $this->result))
+                //    $newObj[$fieldName] = $this->result[$parentNodeName . "." . $fieldName];
+                $newObj[$fieldName] = $resultSet[$parentNodeName . "." . $fieldName];
+
+                //$newObj[$fieldName] = json_encode($this->selectedObjects);
+                //$newObj[$fieldName] = json_encode(self::createManyToManyQuery());
+                //$newObj[$fieldName] = json_encode($this->returnJ)
+
+                // Is the field inside the selectedObjects array as a many-to-many object?
 
                 //$newObj[$fieldName] = $this->getDoctrine()->getRepository($currentDoctrineEntity);
             }
         }
         return $newObj;
+    }
+
+    function getManyToManyAsArray($node, $fieldName, $parentNodeName, $nodeId)
+    {
+        //$selections = $node["selectionSet"]["selections"];
+        $sql = $this->addSqlFields($node);
+        $sql .= " FROM " . $parentNodeName;
+
+        $from = $parentNodeName;
+        $to = $fieldName;
+
+        //return $fieldName . ", " . $parentNodeName;
+
+        $combinedTableName = self::getCombinedTableName($from, $to);
+        $sql .= " INNER JOIN $combinedTableName ON $from" . ".id = $combinedTableName" . "." . $from . "_id";
+        $sql .= " INNER JOIN $to ON $combinedTableName" . "." . $to . "_id = " . $to . ".id";
+
+        $sql .= $this->addIdClause($parentNodeName, $nodeId);
+
+        if (self::hasArgument($node, "first"))
+            $sql .= $this->addLimit(self::getArgumentValue($node, "first"));
+
+        //return $sql;
+
+        //$result = $this->connection->query($sql);
+        $result = $this->queryDb($sql);
+        $resultArray = [];
+
+        while ($row = $result->fetch_assoc()) {
+            array_push($resultArray, $row);
+        }
+
+        //return $sql;
+        //return json_encode($resultArray);
+
+        return count($resultArray) > 0 ? $resultArray : null;
+
+        //return $this->connection->query($sql)->fetch_assoc();
+    }
+
+    function createManyToManyQuery()
+    {
+
     }
 
 
